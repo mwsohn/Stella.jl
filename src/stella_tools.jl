94de0458acf9d2f18e8d6be32c7cd7a808e69ed8
mwsohn@gmail.com <mwsohn@gmail.com>
2017-06-30 23:26:54 -0400

    bug fix

--- src/stella_tools.jl
+++ src/stella_tools.jl
@@ -172,7 +172,7 @@ function tab(df::DataFrame,args::Symbol...; label_dict::Union{Void,Dict} = nothi
                 lblname = label_dict["label"][vs]
                 d = label_dict["value"][lblname]
                 val = names(a,i)
-                a.dicts[i] = Dict()
+                a.dicts[i] = OrderedDict()
                 for j=1:length(val)
                     a.dicts[i][val[j]] = haskey(d,val[j]) ? d[val[j]] : string(val[j])
                 end
