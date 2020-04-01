8258579f936f8712c94fa891f9627b401ae918aa
mwsohn <mwsohn@gmail.com>
2020-04-01 02:05:08 -0400

    Update Stella.jl

--- src/Stella.jl
+++ src/Stella.jl
@@ -26,7 +26,6 @@ export  read_stata,  # read stata 13 and 14 files into DF
         dflist,      # list
         dfmerge,     # merge two dataframes
         dfsample,    # select a sample from a df
-        dfappend,    # stack two df's
         univariate,  # univariate statistics in DF
         tabstat,     # unviariate statistics by subgroups
         substat,     # attach univariate stat to original DF by subgroups
