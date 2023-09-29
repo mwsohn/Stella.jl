d84fa27cd31c1840c2026b1f558a11a25fb5b6c6
mwsohn <mwsohn@gmail.com>
2023-09-29 16:47:56 -0400

    Update DataFrame_tools.jl

--- src/DataFrame_tools.jl
+++ src/DataFrame_tools.jl
@@ -187,6 +187,7 @@ function read_stata(fn::String; chunks::Int=10, read_labels=false)
     numvlabels = 0
     for i in 1:nvar
         valuelabels[i] = strtonull(String(read(fh,len_labelname)))
+        println(valuelabels[i])
 
         # count the number of value labels
         if length(valuelabels[i]) > 0
@@ -311,7 +312,8 @@ function read_stata(fn::String; chunks::Int=10, read_labels=false)
         if length(varlabels[i]) > 0
             variable_dict[varlist[i]] = varlabels[i]
         end
-        if valuelabels[i] != ""
+        println(i)
+        if length(valuelabels[i]) > 0 # != ""
             println(varlist[i], " => ", valuelabels[i])
             lblname_dict[varlist[i]] = Symbol(valuelabels[i])
         end
