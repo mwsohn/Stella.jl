754e9c83a513baac2f756a66a989e949b375561f
mwsohn <mwsohn@gmail.com>
2023-09-30 16:22:48 -0400

    Update tab.jl

--- src/tab.jl
+++ src/tab.jl
@@ -58,6 +58,7 @@ function _tab1(na::NamedArray; decimals = 4, labels = nothing)
     PrettyTables.pretty_table(ar, 
         header=[na.dimnames[1],"Counts","Percent","Cum. Percent"],
         formatters = ft_round(decimals,[3,4]),
+        drop = :none,
         hlines=[0,1,length(rownames),length(rownames)+1],
         vlines=[1])
 end
@@ -101,7 +102,8 @@ function _tab2(na::NamedArray; maxrows = -1, maxcols = 20, labels=nothing)
     pretty_table(d,
         row_labels = rownames2,
         row_label_column_title=string(na.dimnames[1], " / ", na.dimnames[2]),
-        header=colnames, 
+        header=colnames,
+        crop = :none, 
         max_num_of_rows = maxrows,
         max_num_of_columns = maxcols,
         hlines=vcat([0, 1], [x * 3 + 1 for x in 1:(nrow+1)]),
