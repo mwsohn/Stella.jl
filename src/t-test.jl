11f2813aeb6c945b8e7eb54daea8f2d2c0b31920
mwsohn <mwsohn@gmail.com>
2023-09-30 21:28:39 -0400

    Update t-test.jl

--- src/t-test.jl
+++ src/t-test.jl
@@ -278,7 +278,7 @@ function ttest(var::AbstractVector,μ0::Real = 0; table = true, varname = nothin
 
         pretty_table([N MEAN SD SE LB UB],
             header=["N", "Mean", "SD", "SE", "95% LB", "95% UB"],
-            row_labels= varname == nothing ? "" : string(varname),
+            row_labels= [varname == nothing ? "" : string(varname)],
             row_label_column_title="Variable",
             formatters=(ft_printf("%.5f", [2, 3, 4, 5, 6]), ft_printf("%.0f", [1])),
             hlines=[0, 1, 3],
