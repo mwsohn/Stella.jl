d2f7fdb0eb1e4319c8c39fb13a7087a6a63d357d
mwsohn <mwsohn@gmail.com>
2024-01-06 18:04:59 -0500

    Update tab.jl

--- src/tab.jl
+++ src/tab.jl
@@ -23,18 +23,18 @@ function tab(na::NamedArray; skipmissing=true)
 end
 function tab(indf,var::Union{Symbol,String}; decimals=4, skipmissing=true)
     if in(string(var),names(indf)) == false
-        warn(var," is not found in the input DataFrame.")
+        @warn(var," is not found in the input DataFrame.")
         return nothing
     end
     _tab1(freqtable(indf,var, skipmissing=skipmissing); decimals=decimals, labels = vallab(indf,var))
 end
 function tab(indf,var1::Union{Symbol,String},var2::Union{Symbol,String}; maxrows = -1, maxcols = 20, decimals=4, skipmissing=true)
     if in(string(var1),names(indf)) == false
-        warn(var1," is not found in the input DataFrame.")
+        @warn(var1," is not found in the input DataFrame.")
         return nothing
     end
     if in(string(var2), names(indf)) == false
-        warn(var2, " is not found in the input DataFrame.")
+        @warn(var2, " is not found in the input DataFrame.")
         return nothing
     end
     _tab2(freqtable(indf, var1, var2, skipmissing=skipmissing); maxrows=maxrows, maxcols=maxcols, labels=vallab(indf, [var1, var2]), decimals=decimals)
@@ -43,7 +43,7 @@ function tab(indf,var1::Union{Symbol,String},var2::Union{Symbol,String},var3::Un
     maxrows=-1, maxcols=20, decimals=4, skipmissing=true)
     for v in (var1,var2,var3)
         if in(string(v), names(indf))
-            warn(v, " is not found in the input DataFrame.")
+            @warn(v, " is not found in the input DataFrame.")
             return nothing
         end
     end
