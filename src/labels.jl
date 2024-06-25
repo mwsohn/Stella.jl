35ff14b9022d3de0b6769c9fe34af5e767791f86
mwsohn <mwsohn@gmail.com>
2024-06-25 16:03:12 -0400

    Update labels.jl

--- src/labels.jl
+++ src/labels.jl
@@ -10,5 +10,6 @@ function values!(df::DataFrame,v::Union{Symbol,String},valdict::Dict)
     if nonmissingtype(eltype(df[:,v])) <: Integer
         vv = categorical(df[!,v])
         df[!,v] = recode(vv,valdict...)
+        levels!(df[!,v], last.(sort(collect(valdict))))
     end
 end
