d7c8909f7e18fcbf586ad83539b905aab16bf243
mwsohn <mwsohn@gmail.com>
2024-06-25 22:24:45 -0400

    Update alt_operators.jl

--- src/alt_operators.jl
+++ src/alt_operators.jl
@@ -19,36 +19,3 @@ a boolean array that replaces missing values to `false`.
 function lift(a::AbstractArray)
     return coalesce.(a, false)
 end
-
-# alternative operators
-eq(missing, b) = false
-eq(a, missing) = false
-eq(missing, missing) = false
-eq(a::Real, b::Real) = ==(a, b)
-eq(missing, b::AbstractString) = false
-eq(missing, b::Union{Missing,AbstractString}) = false
-eq(a::AbstractString, missing) = false
-eq(a::Union{Missing,AbstractString}, missing) = false
-eq(a::AbstractString, b::AbstractString) = a == b
-eq(a::Union{Missing,AbstractString}, b::Union{Missing,AbstractString}) = a == b
-
-lt(a::Missing, b::Real) = false
-lt(a::Real, b::Missing) = false
-lt(a::Missing, b::Missing) = false
-lt(a::Real, b::Real) = a < b
-
-le(a::Missing, b::Real) = false
-le(a::Real, b::Missing) = false
-le(a::Missing, b::Missing) = false
-le(a::Real, b::Real) = a <= b
-
-gt(a::Missing, b::Real) = false
-gt(a::Real, b::Missing) = false
-gt(a::Missing, b::Missing) = false
-gt(a::Real, b::Real) = a > b
-
-ge(a::Missing, b::Real) = false
-ge(a::Real, b::Missing) = false
-ge(a::Missing, b::Missing) = false
-ge(a::Real, b::Real) = a >= b
-
