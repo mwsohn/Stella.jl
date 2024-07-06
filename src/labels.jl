d8d74a702e3ac7d30f4c944fb9fb1f464dd2851b
mwsohn <mwsohn@gmail.com>
2024-07-06 14:59:55 -0400

    Update labels.jl

--- src/labels.jl
+++ src/labels.jl
@@ -1,14 +1,14 @@
 @reexport import TableMetadataTools
 
-function labels!(df::DataFrame,dd::Dict)
+function labels!(df::AbstractDataFrame,dd::Dict)
     for key in keys(dd)
         label!(df,key,dd[key])
     end
 end
 
-function values!(df::DataFrame,v::Union{Symbol,String},val::Union{Dict,NamedTuple}; ordered = true)
+function values!(df::AbstractDataFrame,v::Union{Symbol,String},val::Union{Dict,NamedTuple}; ordered = true)
     if nonmissingtype(eltype(df[:,v])) <: Integer
-        vv = categorical(df[!,v])
+        vv = categorical(df[!,v], ordered = true)
         df[!,v] = recode(vv,val...)
         levels!(df[!,v], last.(sort(collect(val))))
     end
