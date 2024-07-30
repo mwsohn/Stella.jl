41f16ddb4ffc61ae48f7fa237d80a817659d3cb0
mwsohn <mwsohn@gmail.com>
2024-07-29 22:12:48 -0400

    Update tab.jl

--- src/tab.jl
+++ src/tab.jl
@@ -132,7 +132,7 @@ function _tab2(na::NamedArray; maxrows = -1, maxcols = 20, decimals=4)
 
     if size(testarray) == (2, 2) # 2x2 array
         println("Fisher's exact test = ", @sprintf("%.4f",
-            pvalue(HypothesisTests.FisherExactTest(rec(testarray')...))))
+            pvalue(HypothesisTests.FisherExactTest((testarray')...))))
     end
 end
 
