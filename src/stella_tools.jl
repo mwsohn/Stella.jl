6433c9ad55b21db0fd2b6eef804ccdbc4a926fba
mwsohn <mwsohn@gmail.com>
2024-10-16 10:53:33 -0400

    Update stella_tools.jl

--- src/stella_tools.jl
+++ src/stella_tools.jl
@@ -350,8 +350,10 @@ function eform(glmout::StatsModels.TableRegressionModel)
 
 	# 95% CI
     if isa(glmout.model, CoxModel)
-        push!(coeftable2.cols, exp.(coeftable2.cols[5]))
-        push!(coeftable2.cols, exp.(coeftable2.cols[6]))
+        cv = quantile(Normal(0,1),0.975)
+        push!(coeftable2.cols, exp.(coeftable2.cols[1] .- cv*coeftable2.cols[2]))
+        push!(coeftable2.cols, exp.(coeftable2.cols[1] .+ cv*coeftable2.cols[2]))
+        coeftable2.cols[2] = coeftable2.cols[1] ./ coleftable2.cols[2]
     else
         coeftable2.cols[5] = exp.(coeftable2.cols[5])
         coeftable2.cols[6] = exp.(coeftable2.cols[6])
