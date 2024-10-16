102da363a2186989ee3a187275efbb4e234422e7
mwsohn <mwsohn@gmail.com>
2024-10-16 12:06:34 -0400

    Update stella_tools.jl

--- src/stella_tools.jl
+++ src/stella_tools.jl
@@ -353,7 +353,6 @@ function eform(glmout::StatsModels.TableRegressionModel)
         cv = quantile(Normal(0,1),0.975)
         push!(coeftable2.cols, exp.(coeftable2.cols[1] .- cv .* coeftable2.cols[2])) # 95% CI Lower Bound
         push!(coeftable2.cols, exp.(coeftable2.cols[1] .+ cv .* coeftable2.cols[2])) # 95% CI Upper Bound
-        coeftable2.cols[3] = coeftable2.cols[1] ./ coeftable2.cols[2] # Z-Values
     else
         coeftable2.cols[5] = exp.(coeftable2.cols[5])
         coeftable2.cols[6] = exp.(coeftable2.cols[6])
