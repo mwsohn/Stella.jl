f7b82506bd97be4ed4b2e03ea39552e206b461c4
mwsohn <mwsohn@gmail.com>
2024-10-20 11:25:48 -0400

    Update rocdata.jl

--- src/rocdata.jl
+++ src/rocdata.jl
@@ -3,13 +3,13 @@
 function rocdata(scores, labels)
 
     # function that returns number of trues and falses
-    sumn(v) = (s = sum(v); return (s, length(v) - s))
+    sumnn(v) = (s = sum(v); return (s, length(v) - s))
 
     # thresholds are sorted list of unique values in scores
-    thresholds = vcat(1.0,sort(unique(scores), rev=true))
+    thresholds = sort(vcat(1.0, unique(scores)), rev=true)
 
     # turn labels into a boolean vector
-    labels = labels .== 1.0
+    labels = labels .> 0.0
 
     # total number of positives (trues)
     P = sum(labels)
@@ -52,71 +52,8 @@ end
 
 import Plots.plot
 function plot(rocdata::ROCData)
-    plot(rocdata.FPR, rocdata.TPR, linetype=:steppre, legend=false, xlabel="1 - specificity", ylabel="sensitivity")
-    plot!(collect(0:1:1), collect(0:1:1), legend=false, lc = :black)
+    plt = plot(rocdata.FPR, rocdata.TPR, linetype=:steppre, legend=false, xlabel="1 - specificity", ylabel="sensitivity")
+    plot!(plt, collect(0:1:1), collect(0:1:1), legend=false, lc = :black)
+    return plt
 end
-
-
-# function logrank(df, event, by)
-
-#     # number of groups
-#     # ba = completecases(df[!,[event,by]])
-#     # df2 = df[ba,:]
-#     groups = sort(unique(df[!,by]))
-#     n_groups = length(groups)
-
-#     # perform Kaplan-Meier analysis
-#     km = Vector{KaplanMeier{Float64, Int64}}(undef,n_groups)
-#     times = zeros(Int64,n_groups)
-
-#     for (i,v) in enumerate(groups)
-#         km[i] = fit(KaplanMeier, df[ df[!,by] .== v, event])
-#         times[i] = km[i].events.time[end]
-#     end
-
-#     # lengths
-#     ntimes = maximum(times)
-
-#     # events
-#     events = zeros(Int64, n_groups, ntimes)
-#     for i in 1:n_groups
-#         events[i, km[i].events.time] .= km[i].events.nevents
-#     end
-
-#     # N at risk
-#     atrisk = zeros(Int64, n_groups, ntimes)
-#     for i in 1:n_groups
-#         atrisk[i, 1] = km[i].events.natrisk[1]
-        
-#         for j in 2:ntimes
-#             atrisk[i, j] = atrisk[i, j-1] - events[i, j-1]
-#         end
-#     end
-
-#     # Observed events
-#     O = vec(sum(events, dims=1))
-
-#     # Total N at risk
-#     N = vec(sum(atrisk, dims=1))
-
-#     # Observed Rate
-#     Or = O ./ N
-
-#     # Expected values
-#     E = zeros(Float64, n_groups, ntimes)
-#     for i in 1:n_groups
-#         E[i, :] = Or .* vec(atrisk[i, :])
-#     end
-
-#     o = vec(sum(events, dims=2))
-#     e = vec(sum(E, dims=2))
-
-#     chi2 = sum((o .- e) .^ 2 ./ e)
-
-#     dof = (length(o) - 1) * (length(e) - 1)
-#     pval = ccdf(Chisq(dof), chi2)
-
-#     return (o, e, size(df,1), chi2, dof, pval)
-# end
-
 
