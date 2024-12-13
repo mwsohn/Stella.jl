ba1968ead5fde2d6b4f2e77f644fadbc44f4a0ef
mwsohn <mwsohn@gmail.com>
2024-12-12 23:14:41 -0500

    Update statafiles.jl

--- src/statafiles.jl
+++ src/statafiles.jl
@@ -658,9 +658,9 @@ function write_chunks(outdf,datatypes, typelist)
             elseif datatypes[i] == String
                 write(iobuf,string(v,repeat('\0', typelist[i] - length(v))))
             elseif datatypes[i] == Date
-                write(iobuf, Int32(ismissing(v) ? missingval[typelist[i]] : Dates.value(dt - Date(1960,1,1))))
+                write(iobuf, Int32(ismissing(v) ? missingval[typelist[i]] : Dates.value(v - Date(1960,1,1))))
             elseif datatypes[i] == DateTime
-                write(iobuf, Float64(ismissing(v) ? missingval[typelist[i]] : Dates.value(dt - Date(1960,1,1))))
+                write(iobuf, Float64(ismissing(v) ? missingval[typelist[i]] : Dates.value(v - Date(1960,1,1))))
             else
                 write(iobuf, datatypes[i](ismissing(v) ? missingval[typelist[i]] : unwrap(v)))
             end
