1d0704849d76b358415bbf5e2a09e12af8a3bbad
mwsohn <mwsohn@gmail.com>
2024-12-13 19:04:10 -0500

    Update statafiles.jl

--- src/statafiles.jl
+++ src/statafiles.jl
@@ -659,7 +659,7 @@ function write_chunks(outdf, datatypes, typelist)
         for (i,v) in enumerate(dfrow)
             if isa(outdf[:,i], CategoricalArray)
                 if nonmissingtype(eltype(levels(outdf[:,i]))) == String # output index
-                    write(iobuf, ismissing(v) ? missingval[typelist[i]] : datatypes[i](outdf[:,i].pool.invindex[v])) # refs
+                    write(iobuf, datatypes[i](ismissing(v) ? missingval[typelist[i]] : outdf[:,i].pool.invindex[v])) # refs
                 elseif typelist[i] == 32768 # strLs
                     # not imolemented yet
                 else
@@ -668,9 +668,9 @@ function write_chunks(outdf, datatypes, typelist)
             elseif datatypes[i] == String
                 write(iobuf, ismissing(v) ? repeat('\0', typelist[i]) : string(v, repeat('\0', typelist[i] - length(codeunits(v)))))
             elseif datatypes[i] == Date
-                write(iobuf, ismissing(v) ? missingval[typelist[i]] : datatypes[i](Dates.value(v - Date(1960,1,1))))
+                write(iobuf, datatypes[i](ismissing(v) ? missingval[typelist[i]] : Dates.value(v - Date(1960,1,1))))
             elseif datatypes[i] == DateTime
-                write(iobuf, ismissing(v) ? missingval[typelist[i]] : datatypes[i](Dates.value(v - DateTime(1960,1,1))))
+                write(iobuf, datatypes[i](ismissing(v) ? missingval[typelist[i]] : Dates.value(v - DateTime(1960,1,1))))
             else
                 write(iobuf, datatypes[i](ismissing(v) ? missingval[typelist[i]] : v))
             end
