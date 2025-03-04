8b4d61c08fe73a70a8a9793a99c403f7b3a88813
mwsohn <mwsohn@gmail.com>
2025-03-03 22:02:31 -0500

    Update statafiles.jl

--- src/statafiles.jl
+++ src/statafiles.jl
@@ -359,9 +359,7 @@ function _read_dta(io, release, rlen, len, nvar, varlist, varlabels, typelist, f
         # for integer variables that have formats
         # convert them into CategoricalArrays with the appropriate value labels
         if typelist[j] in (65528, 65529, 65530) && haskey(lblname, j)
-            # Stella.values!(df, varlist[j], vallabels[lblname[j]])
-            df[!,varlist[j]] = recode(df[!,varlist[j]], vallabels[lblname[j]]...)
-            df[!,varlist[j]] = categorical(df[!,varlist[j]])
+            Stella.values!(df, varlist[j], vallabels[lblname[j]], ordered=true)
         end
 
         # variable label
