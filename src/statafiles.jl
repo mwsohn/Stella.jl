4f1ae8ee2a638af1be44a5c4602cae231b2fbd67
mwsohn <mwsohn@gmail.com>
2025-03-05 09:58:55 -0500

    Update statafiles.jl

--- src/statafiles.jl
+++ src/statafiles.jl
@@ -265,7 +265,7 @@ function read_stata(fn::String; chunks::Int=10)
 
         # for integer variables that have formats
         # convert them into CategoricalArrays with the appropriate value labels
-        if typelist[j] in (65528, 65529, 65530) && haskey(lblname, j)
+        if typelist[j] in (65528, 65529, 65530) && haskey(lblname_dict, j)
             rdf[!,varlist[j]] = categorical(recode(rdf[!,varlist[j]], value_labels[lblname_dict[j]]...))
         end
 
