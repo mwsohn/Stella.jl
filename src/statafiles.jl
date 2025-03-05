91456020bfa40bbe50ace89819fdcfd34f24937f
mwsohn <mwsohn@gmail.com>
2025-03-05 12:47:03 -0500

    Update statafiles.jl

--- src/statafiles.jl
+++ src/statafiles.jl
@@ -267,8 +267,10 @@ function read_stata(fn::String; chunks::Int=10)
         # convert them into CategoricalArrays with the appropriate value labels
         if typelist[i] in (65528, 65529, 65530) && haskey(lblname_dict, i)
             # Stella.values!(rdf, varlist[i], value_labels[lblname_dict[i]])
+            tmpvec = recode(rdf[!,varlist[i]], value_labels[lblname_dict[i]]...)
+            eltype(tmpvec)
                       
-            rdf[!,:tmpvar] = categorical(recode(rdf[!,varlist[i]], value_labels[lblname_dict[i]]...), ordered = true)
+            rdf[!,:tmpvar] = categorical(tmpvec, ordered = true)
         end
 
         # variable label
