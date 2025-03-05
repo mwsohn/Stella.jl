7cac6541782410650d81d15681541f78b45e7d28
mwsohn <mwsohn@gmail.com>
2025-03-05 00:04:06 -0500

    Update statafiles.jl

--- src/statafiles.jl
+++ src/statafiles.jl
@@ -359,8 +359,7 @@ function _read_dta(io, release, rlen, len, nvar, varlist, varlabels, typelist, f
         # for integer variables that have formats
         # convert them into CategoricalArrays with the appropriate value labels
         if typelist[j] in (65528, 65529, 65530) && haskey(lblname, j)
-            df[!,varlist[j]] = recode(df[!,varlist[j]], vallabels[lblname[j]]...)
-            df[!,varlist[j]] = CategoricalArray(df[!,varlist[j]])
+            df[!,varlist[j]] = categorical(recode(df[!,varlist[j]], vallabels[lblname[j]]...))
         end
 
         # variable label
