6405ce09b3a21e8d794408c457d0181b8348045f
mwsohn <mwsohn@gmail.com>
2025-03-05 13:59:16 -0500

    Update statafiles.jl

--- src/statafiles.jl
+++ src/statafiles.jl
@@ -271,9 +271,7 @@ function read_stata(fn::String; chunks::Int=10)
         # for integer variables that have formats
         # convert them into CategoricalArrays with the appropriate value labels
         if typelist[i] in (65528, 65529, 65530) && haskey(lblname_dict, i)
-            # Stella.values!(rdf, varlist[i], value_labels[lblname_dict[i]])
-            rdf[!, varlist[i]] = recode(rdf[!,varlist[i]], value_labels[lblname_dict[i]]...)
-            # rdf[!,varlist[i]] = CategoricalArray{Union{Missing,String}}()
+            rdf[!, varlist[i]] = categorical(recode(rdf[!,varlist[i]], value_labels[lblname_dict[i]]...), ordered = ordered, compressed = true)
         end
 
         # variable label
