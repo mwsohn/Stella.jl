0eeda5ccd3684801040c8bfc7cecc87d3f19754f
mwsohn <mwsohn@gmail.com>
2025-03-05 12:53:42 -0500

    Update statafiles.jl

--- src/statafiles.jl
+++ src/statafiles.jl
@@ -268,7 +268,7 @@ function read_stata(fn::String; chunks::Int=10)
         if typelist[i] in (65528, 65529, 65530) && haskey(lblname_dict, i)
             # Stella.values!(rdf, varlist[i], value_labels[lblname_dict[i]])
             tmpvec = recode(rdf[!,varlist[i]], value_labels[lblname_dict[i]]...)
-            rdf[!,varlist[i]] = CategoricalArray{Union{Missing,String},UInt32}(tmpvec, ordered = true)
+            rdf[!,varlist[i]] = CategoricalArray{Union{Missing,String},UInt32}(tmpvec)
         end
 
         # variable label
