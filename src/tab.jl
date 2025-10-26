d1f0c65eb259676a2333e548a99b0315705dd92f
mwsohn <mwsohn@gmail.com>
2025-10-26 03:41:57 -0400

    Update tab.jl

--- src/tab.jl
+++ src/tab.jl
@@ -190,9 +190,13 @@ function _tab1summarize(indf,var,sumvar; skipmissing = false)
     else
         ba = completecases(indf[!,[sumvar]])
     end
-    indf2 = indf[ba,[var,sumvar]]
+    if sort 
+        indf2 = sort(indf[ba,[var,sumvar]],var)
+    else
+        indf2 = indf[ba,[var,sumvar]]
+    end
 
-    odf = combine(groupby(indf2,var,sort=true), nrow => :n, sumvar => mean => :mean, sumvar => std => :sd)
+    odf = combine(groupby(indf2,var), nrow => :n, sumvar => mean => :mean, sumvar => std => :sd)
     rownames = vcat(string.(odf[!,var]),"Total")
     tdf = DataFrame(n = nrow(indf2), mean = mean(indf2[!,sumvar]), sd = std(indf2[!,sumvar]))
     odf = vcat(odf[!,2:end],tdf)
