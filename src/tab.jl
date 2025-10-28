7634cbcf75180ff951999fdb262dc36945b81494
mwsohn <mwsohn@gmail.com>
2025-10-27 23:53:48 -0400

    Update tab.jl

--- src/tab.jl
+++ src/tab.jl
@@ -257,7 +257,7 @@ function _tab2summarize(indf, var1, var2, sumvar; maxrows=-1, maxcols=20, skipmi
         row_label_column_title=string(var1, " / ", var2),
         header=colnames,
         crop=:none,
-        formatters=[x -> isa(x, AbstractFloat) ? @sprintf("%.2f", x) : string(x)],
+        formatters=[(x,i,j) -> isa(x, AbstractFloat) ? @sprintf("%.2f", x) : string(x)],
         max_num_of_rows=maxrows,
         max_num_of_columns=maxcols,
         hlines=vcat([0, 1], [x * 3 + 1 for x in 1:(nrows + 1)]),
