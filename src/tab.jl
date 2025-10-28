52355eb33b9e4bfddebb0dd9a02e993e5160d9c7
mwsohn <mwsohn@gmail.com>
2025-10-27 23:34:50 -0400

    Update tab.jl

--- src/tab.jl
+++ src/tab.jl
@@ -257,14 +257,14 @@ function _tab2summarize(indf, var1, var2, sumvar; maxrows=-1, maxcols=20, skipmi
         row_label_column_title=string(var1, " / ", var2),
         header=colnames,
         crop=:none,
-        formatters = f_formatter,
+        formatters = [f_formatter],
         max_num_of_rows=maxrows,
         max_num_of_columns=maxcols,
         hlines=vcat([0, 1], [x * 3 + 1 for x in 1:(nrows + 1)]),
         vlines=[1])
 end
 
-function f_formatter(x)
+f_formatter = x -> begin
     if isa(x, AbstractFloat)
         @sprintf("%.2f", float(x))
     else
