42e106094314efefb588e7734c24302d12eecba7
mwsohn <mwsohn@gmail.com>
2026-04-13 01:27:10 -0400

    Update stella_tools.jl

--- src/stella_tools.jl
+++ src/stella_tools.jl
@@ -115,7 +115,7 @@ function univ(v::AbstractVector; table=true)
                 horizontal_line_at_beginning = true,
                 horizontal_line_after_column_labels = true,
                 horizontal_line_after_data_rows = true,
-                horizontal_line_at_merged_column_labels = false
+                first_line_merged_column_label = [crayon"underline",crayon"underline"]
             )
         )
         return nothing
